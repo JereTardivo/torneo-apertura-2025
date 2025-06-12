47aacb84c11075c1b03da8c2889c78f51341ffad
Jeremias Tardivo <jeretardivo1992@gmail.com>
2025-06-12 18:54:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -960,11 +960,11 @@ const fechasFijas = {
                         resultadoHTML = `
                     <input type="text" inputmode="numeric" pattern="[0-9]*" min="0" oninput="this.value = this.value.replace(/[^0-9]/g, \'\');" value="${partido.goles_local === "" ? "" : partido.goles_local}" 
                         onchange="updateResultado('${fechaNombre}', ${index}, 'local', this.value)"
-                        style="width: 40px; text-align: center; background: white; color: black;"> 
+                        class="w-10 md:w-12 text-center bg-white text-black"> 
                     - 
                     <input type="text" inputmode="numeric" pattern="[0-9]*" min="0" oninput="this.value = this.value.replace(/[^0-9]/g, \'\');" value="${partido.goles_visitante === "" ? "" : partido.goles_visitante}" 
                         onchange="updateResultado('${fechaNombre}', ${index}, 'visitante', this.value)"
-                        style="width: 40px; text-align: center; background: white; color: black;">`;
+                        class="w-10 md:w-12 text-center bg-white text-black">`;
                     } else {
                         resultadoHTML = `<span class="font-bold text-base">${partido.goles_local} - ${partido.goles_visitante}</span>`;
                     }
