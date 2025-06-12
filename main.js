cdd64f6780503345bafad80657042cac7055a9db
Jeremias Tardivo <jeretardivo1992@gmail.com>
2025-06-12 19:27:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -959,18 +959,18 @@ const fechasFijas = {
                     let resultadoHTML = "";
 
                     if (editable) {
-                        resultadoHTML = `
+                        resultadoHTML = ``
 <input type="text" inputmode="numeric" pattern="[0-9]*" min="0"
     oninput="this.value = this.value.replace(/[^0-9]/g, '');"
-    value='\${partido.goles_local === "" ? "" : partido.goles_local}'
-    onchange="updateResultado('\${fecha}', \${index}, 'local', this.value)"
+    value='${partido.goles_local === "" ? "" : partido.goles_local}'
+    onchange="updateResultado('${fechaNombre}', ${index}, 'local', this.value)"
     class="w-10 md:w-12 text-center bg-white text-black">
 -
 <input type="text" inputmode="numeric" pattern="[0-9]*" min="0"
     oninput="this.value = this.value.replace(/[^0-9]/g, '');"
-    value='\${partido.goles_visitante === "" ? "" : partido.goles_visitante}'
-    onchange="updateResultado('\${fecha}', \${index}, 'visitante', this.value)"
-    class="w-10 md:w-12 text-center bg-white text-black">`;
+    value='${partido.goles_visitante === "" ? "" : partido.goles_visitante}'
+    onchange="updateResultado('${fechaNombre}', ${index}, 'visitante', this.value)"
+    class="w-10 md:w-12 text-center bg-white text-black">`;`;
                     } else {
                         resultadoHTML = `
 <input type="text" inputmode="numeric" pattern="[0-9]*" min="0"
